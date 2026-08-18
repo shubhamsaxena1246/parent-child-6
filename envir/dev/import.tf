2d3b27bb0bd4cea20d8abfb4d101427754f74d6f
shubhamsaxena1246 <shubhamsaxena1246@gmail.com>
2026-08-18 21:53:38 +0530

    Resolve VM Terraform conflict and remove provisioners

--- envir/dev/import.tf
+++ envir/dev/import.tf
@@ -1,43 +1,43 @@
-import {
-  to = module.example.azurerm_resource_group.example["rg1"]
+# import {
+#   to = module.example.azurerm_resource_group.example["rg1"]
 
-  id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp"
-}
+#   id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp"
+# }
 
-import {
-  to = module.pip.azurerm_public_ip.pip["pip1"]
+# import {
+#   to = module.pip.azurerm_public_ip.pip["pip1"]
 
-  id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/publicIPAddresses/pip_vm_1"
-}
+#   id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/publicIPAddresses/pip_vm_1"
+# }
 
-import {
-  to = module.vnet.azurerm_virtual_network.vnet["vnet1"]
+# import {
+#   to = module.vnet.azurerm_virtual_network.vnet["vnet1"]
 
-  id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/virtualNetworks/vnet"
-}
-import {
-  to = module.snet.azurerm_subnet.snet["snet1"]
+#   id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/virtualNetworks/vnet"
+# }
+# import {
+#   to = module.snet.azurerm_subnet.snet["snet1"]
 
-  id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/virtualNetworks/vnet/subnets/subnet_frontend"
-}
-import {
-  to = module.main.azurerm_network_interface.nic["vms1"]
+#   id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/virtualNetworks/vnet/subnets/subnet_frontend"
+# }
+# import {
+#   to = module.main.azurerm_network_interface.nic["vms1"]
 
-  id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/networkInterfaces/nic_1"
-}
+#   id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/networkInterfaces/nic_1"
+# }
 
-import {
-  to = module.main.azurerm_network_security_group.nsg["vms1"]
+# import {
+#   to = module.main.azurerm_network_security_group.nsg["vms1"]
 
-  id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/networkSecurityGroups/frontend-nsg"
-}
-import {
-  to = module.main.azurerm_network_interface_security_group_association.nsg_assoc["vms1"]
+#   id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/networkSecurityGroups/frontend-nsg"
+# }
+# import {
+#   to = module.main.azurerm_network_interface_security_group_association.nsg_assoc["vms1"]
 
-  id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/networkInterfaces/nic_1|/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/networkSecurityGroups/frontend-nsg"
-}
-import {
-  to = module.main.azurerm_virtual_machine.main["vms1"]
+#   id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/networkInterfaces/nic_1|/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/networkSecurityGroups/frontend-nsg"
+# }
+# import {
+#   to = module.main.azurerm_virtual_machine.main["vms1"]
 
-  id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Compute/virtualMachines/Fronetend_Vm"
-}
+#   id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Compute/virtualMachines/Fronetend_Vm"
+# }
