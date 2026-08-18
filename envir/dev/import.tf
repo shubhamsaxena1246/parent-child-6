9dd76b98ca4b0cdf637f74e7277af941d2356e89
shubhamsaxena1246 <shubhamsaxena1246@gmail.com>
2026-08-18 21:37:47 +0530

    Resolve VM Terraform conflict and remove provisioners

--- envir/dev/import.tf
+++ envir/dev/import.tf
@@ -3,3 +3,15 @@ import {
 
   id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp"
 }
+
+import {
+  to = module.pip.azurerm_public_ip.pip["pip1"]
+
+  id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/publicIPAddresses/pip_vm_1"
+}
+
+import {
+  to = module.vnet.azurerm_virtual_network.vnet["vnet1"]
+
+  id = "/subscriptions/a60bfb4b-160f-44e7-979b-775bdd787c90/resourceGroups/rg-paratp/providers/Microsoft.Network/virtualNetworks/vnet"
+}
